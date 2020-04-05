929ee5f33b4274a7c0f2e046953138ffcba9479b
Ilya <Shirshov.ilya@yandex.ru>
2020-04-05 19:58:07 +0300

    task_1

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log(6);
+"use strict";
+let money = +prompt( "Ваш бюджет на месяц?" );
+let time = prompt( "Введите дату в формате YYYY-MM-DD" ); 
+let appData = {
+    budgetData : money,
+    timeData : time,
+    expenses : {},
+    optionalExpenses : {},
+    income : [],
+    saving : false,
+};
+let question = prompt("Введите обязательную статью расходов в этом месяце");
+let moneyQuestion = +prompt("Во сколько обойдется?");
+expenses :{
+    question : moneyQuestion
+}
+alert ("Бюджет на 1 день"+appData.budgetData/30);
